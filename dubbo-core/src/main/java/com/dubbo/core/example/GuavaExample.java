5bcf6686ee1c5a251389cf7c30ba289a116fcc7b
lizixiang <842089383@qq.com>
2022-07-21 21:25:01 +0800

    guava first commit

---
+++ dubbo-core/src/main/java/com/dubbo/core/example/GuavaExample.java
@@ -0,0 +1,12 @@
+package com.dubbo.core.example;
+
+import com.google.common.cache.CacheBuilder;
+
+public class GuavaExample {
+
+    public static void main(String[] args) {
+//        CacheBuilder.newBuilder()
+//                .concurrencyLevel().build();
+    }
+
+}
