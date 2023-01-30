97ae52d9cd7bd78c28760f60fe17635903e679cb
lizixiang <842089383@qq.com>
2023-01-30 12:04:15 +0800

    dubbo-design init

---
+++ dubbo-design/src/main/java/com/dubbo/design/DubboDesignApplication.java
@@ -0,0 +1,13 @@
+package com.dubbo.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboDesignApplication.class, args);
+    }
+
+}
